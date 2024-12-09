700539b7103221e5e6309d3ec02c95aa65ae8d6f
MahmoudMohsenEgy <MahmoudMohsenEgy@gmail.com>
2024-12-09 14:18:48 +0200

    changing chatpdf file to class structure

--- chatpdf1.py
+++ chatpdf1.py
@@ -15,28 +15,40 @@ from modules.embedding_model.embedding_model import EmbeddingModel
 from modules.cache.semantic_cache import SemanticCache
 from modules.cost_calculator import CostCalculator
 from credentials import API_KEY
-genai.configure(api_key=API_KEY)
 
+class ChatPDFApp:
+    def __init__(self):
+        genai.configure(api_key=API_KEY)
+        self.vdb_utils = QdrantVectorDB(collection_name="doc_vb", host="192.168.1.110")
+        self.semantic_cache = SemanticCache(EmbeddingModel().get_embeddings(), threshold=0.75)
+        self.vector_store = self.vdb_utils.load_vector_store(EmbeddingModel().get_embeddings())
+        self.model = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=API_KEY)
 
-vdb_utils = QdrantVectorDB(collection_name="doc_vb", host="192.168.1.110")
-semantic_cache = SemanticCache(EmbeddingModel().get_embeddings(), threshold=0.75)
-vector_store = vdb_utils.load_vector_store(EmbeddingModel().get_embeddings())
-# Function to handle user input and generate answers
-def user_input(user_question, embeddings):
-    """Handles the user's input, decides whether to use cached or fresh response."""
-    start_time = time.time()
-    response = semantic_cache.ask(user_question)
-    if response:
-        st.write(f"Cached answer: {response}")
-    else:
-        
-        # Get relevant context using VectorDatabaseUtils
-        context = vdb_utils.get_relevant_context(user_question, vector_store)
-    
-        # print("Context: ", context)
-        # If context is not empty, pass it into the prompt
+    def user_input(self, user_question, embeddings):
+        """Handles the user's input, decides whether to use cached or fresh response."""
+        start_time = time.time()
+        response = self.semantic_cache.ask(user_question)
+        if response:
+            st.write(f"Cached answer: {response}")
+        else:
+            context = self.vdb_utils.get_relevant_context(user_question, self.vector_store)
+            prompt_template = self.get_prompt_template(context, user_question)
+            response = self.model.predict(prompt_template)
+            self.semantic_cache.add_to_cache(user_question, response)
+            st.write("Reply (from Gemini Pro): ", response)
+
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        cost, total_tokens = CostCalculator.calculate_cost("gemini-pro", user_question, response)
+
+        st.write(f"Time taken: {elapsed_time:.2f} seconds")
+        st.write(f"Total tokens: {total_tokens}")
+        st.write(f"Estimated cost: ${cost:.4f}")
+
+    def get_prompt_template(self, context, user_question):
+        """Generates the appropriate prompt template based on context availability."""
         if context and context != []:
-            prompt_template = """You are a helpful and knowledgeable AI assistant. Your task is to provide accurate and comprehensive answers to questions while distinguishing whether the response is based on the provided context or your general knowledge.
+            return f"""You are a helpful and knowledgeable AI assistant. Your task is to provide accurate and comprehensive answers to questions while distinguishing whether the response is based on the provided context or your general knowledge.
 
                 Important Instructions:
                 1. If the answer is based on the provided context, clearly indicate this by starting with:
@@ -49,14 +61,13 @@ def user_input(user_question, embeddings):
                 - If the question is in English, answer entirely in English.
                 - If the question is in Arabic, answer entirely in Egyptian Arabic.
                 4. Be thorough and ensure clarity in your response.
-
 
             Context: {context}
             Question: {user_question}
             
             Answer: """
         else:
-            prompt_template = """You are a helpful and knowledgeable AI assistant. Your task is to provide accurate and comprehensive answers to questions while distinguishing whether the response is based on the provided context or your general knowledge.
+            return f"""You are a helpful and knowledgeable AI assistant. Your task is to provide accurate and comprehensive answers to questions while distinguishing whether the response is based on the provided context or your general knowledge.
 
                 Important Instructions:
                 1. If the answer is based on the provided context, clearly indicate this by starting with:
@@ -70,66 +81,41 @@ def user_input(user_question, embeddings):
                 - If the question is in Arabic, answer entirely in Egyptian Arabic.
                 4. Be thorough and ensure clarity in your response.
 
-
             Question: {user_question}
             
             Answer: """
-        
-        model = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=API_KEY)
-        response = model.predict(prompt_template.format(
-            context=context,
-            user_question=user_question
-        ))
-        # print("Response: ", response)
-        semantic_cache.add_to_cache(user_question, response)
-        st.write("Reply (from Gemini Pro): ", response)
 
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    cost, total_tokens = CostCalculator.calculate_cost("gemini-pro", user_question, response)
+    def main(self):
+        st.set_page_config(page_title="Chat with PDF")
+        st.header("Chat with PDF using Gemini 💬")
 
-    st.write(f"Time taken: {elapsed_time:.2f} seconds")
-    st.write(f"Total tokens: {total_tokens}")
-    st.write(f"Estimated cost: ${cost:.4f}")
+        if "vector_store_created" not in st.session_state:
+            st.session_state["vector_store_created"] = False
 
+        with st.sidebar:
+            st.title("Upload PDF")
+            pdf_docs = st.file_uploader("Upload your PDF Files", accept_multiple_files=True)
 
-# Main Streamlit app
-def main():
-    st.set_page_config(page_title="Chat with PDF")
-    st.header("Chat with PDF using Gemini 💬")
+            if st.button("Process PDFs"):
+                if pdf_docs:
+                    with st.spinner("Processing..."):
+                        raw_text = DocUtils.get_pdf_text(pdf_docs)
+                        text_chunks = DocUtils.get_text_chunks(raw_text)
+                        embeddings = EmbeddingModel().get_embeddings()
+                        self.vdb_utils.create_vector_store(text_chunks, embeddings)
+                        st.session_state["vector_store_created"] = True
+                        st.success("PDFs processed and vector store created!")
+                else:
+                    st.warning("Please upload PDF files before processing.")
 
-    # Initialize session state
-    if "vector_store_created" not in st.session_state:
-        st.session_state["vector_store_created"] = False
+        user_question = st.text_input("Ask a question about the uploaded PDFs")
 
-    # File uploader for PDF files
-    with st.sidebar:
-        st.title("Upload PDF")
-        pdf_docs = st.file_uploader("Upload your PDF Files", accept_multiple_files=True)
-
-        if st.button("Process PDFs"):
-            if pdf_docs:
-                with st.spinner("Processing..."):
-                    # Use utility classes for processing
-                    raw_text = DocUtils.get_pdf_text(pdf_docs)
-                    text_chunks = DocUtils.get_text_chunks(raw_text)
-                    embeddings = EmbeddingModel().get_embeddings()
-                    vdb_utils.create_vector_store(text_chunks, embeddings)
-                    st.session_state["vector_store_created"] = True
-                    st.success("PDFs processed and vector store created!")
-            else:
-                st.warning("Please upload PDF files before processing.")
-
-    # User question input
-    user_question = st.text_input("Ask a question about the uploaded PDFs")
-
-    # Handle user questions
-    if user_question and st.session_state["vector_store_created"]:
-        embeddings = EmbeddingModel().get_embeddings()
-        user_input(user_question, embeddings)
-    elif user_question:
-        st.warning("Please process the PDFs first before asking questions.")
-
+        if user_question and st.session_state["vector_store_created"]:
+            embeddings = EmbeddingModel().get_embeddings()
+            self.user_input(user_question, embeddings)
+        elif user_question:
+            st.warning("Please process the PDFs first before asking questions.")
 
 if __name__ == "__main__":
-    main()
+    app = ChatPDFApp()
+    app.main()
