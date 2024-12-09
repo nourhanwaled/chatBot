c83b1185dbaa99f1af787f672ab9e3818b343e96
MahmoudMohsenEgy <MahmoudMohsenEgy@gmail.com>
2024-12-09 13:56:32 +0200

    credentials_file

---
+++ credentials.py
@@ -0,0 +1,12 @@
+
+
+import os
+
+API_KEY = os.getenv('API_KEY')
+
+if API_KEY:
+    print("API Key found")
+else:
+    print("API Key not set")
+    
+API_KEY = "AIzaSyDD_tB4agn1XgLY1myGIfWV0oSG3q9IA3w"
